7e7022ce412b7283601c01be8833045bcb08be76
joamag <joamag@gmail.com>
2019-03-26 23:22:42 +0000

    Initial post support

--- js/api/api.js
+++ js/api/api.js
@@ -1,22 +1,60 @@
 import { Observable } from "./observable";
+import { urlEncode } from "../util";
 import fetch from "node-fetch";
 
 export class API extends Observable {
     async get(
         url,
-        headers = null,
         params = null,
-        handle = null,
-        silent = null,
-        redirect = null,
-        timeout = null,
-        extra = null
+        headers = null,
+        kwargs = {}
     ) {
+        const handle = kwargs.handle !== undefined ? kwargs.handle : true;
+        const query = urlEncode(params || {});
+        if (query) url += url.includes("?") ? "&" + query : "?" + query;
         const response = await fetch(url, {
             method: "GET",
             headers: headers || {}
         });
-        const result = await this._handleResponse(response);
+        const result = handle ? await this._handleResponse(response) : response;
+        return result;
+    }
+
+    async post(
+        url,
+        params = null,
+        data = null,
+        dataJ = null,
+        dataM = null,
+        headers = null,
+        mime = null,
+        extra = {}
+    ) {
+        const handle = extra.handle !== undefined ? extra.handle : true;
+        const query = urlEncode(params || {});
+
+        if (data !== null) {
+            if (query) url += url.includes("?") ? "&" + query : "?" + query;
+        } else if (dataJ !== null) {
+            data = JSON.stringify(dataJ);
+            if (query) url += url.includes("?") ? "&" + query : "?" + query;
+            mime = mime || "application/json";
+        } else if (dataM !== null) {
+            if (query) url += url.includes("?") ? "&" + query : "?" + query;
+        } else if (query) {
+            data = query;
+            mime = mime || "application/x-www-form-urlencoded";
+        }
+
+        headers = Object.assign({}, headers);
+        if (mime) headers["Content-Type"] = mime;
+
+        const response = await fetch(url, {
+            method: "POST",
+            headers: headers || {},
+            body: data
+        });
+        const result = handle ? await this._handleResponse(response) : response;
         return result;
     }
 
--- test/api/api.js
+++ test/api/api.js
@@ -1,12 +1,46 @@
 const assert = require("assert");
 const ripe = require("../..");
 
+const httpbinUrl = "https://httpbin.stage.hive.pt/";
+
 describe("API", function() {
     describe("#constructor()", function() {
         it("should be able to contruct a new API instance", () => {
             const api = new ripe.API();
             assert.notStrictEqual(api, null);
             assert.notStrictEqual(api, undefined);
+        });
+    });
+    describe("#get()", function() {
+        it("should be able to run a simple get", async () => {
+            let result;
+            const api = new ripe.API();
+
+            result = await api.get(httpbinUrl + "ip");
+            assert.notStrictEqual(result.origin, null);
+            assert.notStrictEqual(result.origin, undefined);
+
+            result = await api.get(httpbinUrl + "get", { hello: "world" });
+            assert.deepStrictEqual(result.args, { hello: "world" });
+
+            result = await api.get(httpbinUrl + "get", { hello: "world" }, { hello: "world" });
+            assert.deepStrictEqual(result.args, { hello: "world" });
+            assert.strictEqual(result.headers.Hello, "world");
+        });
+    });
+    describe("#post()", function() {
+        it("should be able to run a simple post", async () => {
+            let result;
+            const api = new ripe.API();
+
+            result = await api.post(httpbinUrl + "post", { hello: "world" });
+            assert.deepStrictEqual(result.form, { hello: "world" });
+
+            result = await api.post(httpbinUrl + "post", undefined, "hello world");
+            assert.strictEqual(result.data, "hello world");
+
+            result = await api.post(httpbinUrl + "post", undefined, undefined, { hello: "world" });
+            assert.deepStrictEqual(result.json, { hello: "world" });
         });
     });
 });
