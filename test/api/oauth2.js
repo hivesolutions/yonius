2d8351dd7cfeb294a3664b7e98e25068fc2cdce1
joamag <joamag@hive.pt>
2019-03-26 13:33:44 +0000

    New test values

---
+++ test/api/oauth2.js
@@ -0,0 +1,11 @@
+const assert = require("assert");
+const ripe = require("../..");
+
+describe("OAuth2API", function() {
+    describe("#constructor()", function() {
+        it("should be able to contruct a new OAuth2 API instance", () => {
+            const api = new ripe.OAuth2API();
+            assert.notStrictEqual(api, null);
+        });
+    });
+});
