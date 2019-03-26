5783f2a57bed24d6c4b2edc37d21e5374d483af0
joamag <joamag@hive.pt>
2019-03-26 15:58:12 +0000

    Re-added babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import pkg from "./package.json";
@@ -29,7 +30,10 @@ export default [
         },
         plugins: [
             resolve(),
-            commonjs()
+            commonjs(),
+            babel({
+                exclude: "node_modules/**"
+            })
         ]
     },
     {
