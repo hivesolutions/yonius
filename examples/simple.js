8d52413310652f7a03db8b0ef256a0d4c39d5a3b
joamag <joamag@hive.pt>
2019-03-26 13:18:34 +0000

    Small example values

--- examples/simple.js
+++ examples/simple.js
@@ -2,7 +2,8 @@ const yonius = require("..");
 
 async function run() {
     const api = new yonius.API();
-    api.trigger("example");
+    api.bind("example", value => console.info(value));
+    api.trigger("example", "hello");
 }
 
 run();
