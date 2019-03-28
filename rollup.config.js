b6b44812b80bd6552b4ac0f259a9f9d44d4e9afc
joamag <joamag@hive.pt>
2019-03-28 10:33:15 +0000

    New globals support

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+import globals from "rollup-plugin-node-globals";
 import builtins from "rollup-plugin-node-builtins";
 import pkg from "./package.json";
 
@@ -30,6 +31,7 @@ export default [
             }
         },
         plugins: [
+            globals(),
             builtins(),
             resolve({
                 customResolveOptions: {
