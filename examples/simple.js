005067ec6f8377c87f62bb0ab8102cb4f550b1c7
joamag <joamag@hive.pt>
2019-03-28 09:58:53 +0000

    Small change

--- examples/simple.js
+++ examples/simple.js
@@ -1,7 +1,7 @@
 const yonius = require("..");
 
 async function run() {
-    console.info(yonius.conf("HOME"));
+    console.info(yonius.conf("OS"));
     const api = new yonius.API();
     api.bind("example", value => console.info(value));
     api.trigger("example", "hello");
