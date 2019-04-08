25c5fd49d0f9fcc2373a7f2fc30f402c65f07377
joamag <joamag@gmail.com>
2019-04-08 21:42:38 +0100

    Better OAuth 2 support

--- js/api/oauth2.js
+++ js/api/oauth2.js
@@ -1,5 +1,40 @@
 import { OAuthAPI } from "./oauth";
 
-export class OAuth2API extends OAuthAPI {}
+export class OAuth2API extends OAuthAPI {
+    constructor(kwargs = {}) {
+        super(kwargs);
+        this.accessToken = null;
+    }
+
+    async build(method, url, options = {}) {
+        const headers = options.headers !== undefined ? options.headers : {};
+        const kwargs = options.kwargs !== undefined ? options.kwargs : {};
+        const token = kwargs.token === undefined ? this.tokenDefault : kwargs.token;
+        delete kwargs.token;
+        if (token && this.oauthType.includes("param")) {
+            kwargs[this.oauthParam] = this.getAccessToken();
+        }
+        if (token && self.oauthTypes.includes("header")) {
+            headers.Authorization = `Bearer ${this.getAccessToken()}`;
+        }
+    }
+
+    getAccessToken() {
+        if (this.accessToken) return self.access_token;
+        throw new Error("No access token found must re-authorize");
+    }
+
+    get oauthTypes() {
+        return ["param", "header"];
+    }
+
+    get oauthParam() {
+        return "accessToken";
+    }
+
+    get tokenDefault() {
+        return true;
+    }
+}
 
 export default OAuth2API;
