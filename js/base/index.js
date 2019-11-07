976364056d6be4926667498e65e1d48119129760
joamag <joamag@gmail.com>
2019-11-07 17:23:44 +0000

    New ensure support

--- js/base/index.js
+++ js/base/index.js
@@ -1,3 +1,3 @@
-export { ensurePermissions, ensurePermissions as ensure } from "./auth";
+export { ensurePermissions } from "./auth";
 export { YoniusError, OperationalError, NotFoundError, NotImplementedError } from "./error";
 export { load, unload, register, unregister, request } from "./main";
