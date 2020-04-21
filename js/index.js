69956d72a2255ef4d3fd524898daf6d85f726d46
joamag <joamag@hive.pt>
2020-04-21 22:29:24 +0100

    Exporting of name and version

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
+import { name, version } from "..//package.json";
+
 export * from "./api";
 export * from "./base";
 export * from "./data";
 export * from "./express";
 export * from "./util";
+
+export * from "./js";
+export * from "./vue";
+
+export const NAME = name;
+export const VERSION = version;
