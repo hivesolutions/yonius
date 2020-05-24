3affd01f3afa6add7d7443d0ac7d1f18de2c62c6
joamag <joamag@hive.pt>
2020-05-24 09:15:00 +0100

    Fixed rollup and json

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import json from "rollup-plugin-json";
+import json from "@rollup/plugin-json";
 import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
