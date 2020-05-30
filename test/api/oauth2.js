213329352d78152f91d782f9e161a5d3e5ba90de
joamag <joamag@hive.pt>
2020-05-30 10:57:56 +0100

    Spell fix

--- test/api/oauth2.js
+++ test/api/oauth2.js
@@ -3,7 +3,7 @@ const yonius = require("../..");
 
 describe("OAuth2API", function() {
     describe("#constructor()", function() {
-        it("should be able to contruct a new OAuth2 API instance", () => {
+        it("should be able to construct a new OAuth2 API instance", () => {
             const api = new yonius.OAuth2API();
             assert.notStrictEqual(api, null);
             assert.notStrictEqual(api, undefined);
