fad7d0230030c8e923cf78246547919195514dd9
joamag <joamag@hive.pt>
2020-06-02 19:44:22 +0100

    New export function definition

--- types/util/config.d.ts
+++ types/util/config.d.ts
@@ -1,1 +1,2 @@
+export declare function load(names?: Array<string>, path?: string, encoding?: string, force?: boolean, ctx?: any): Promise<any>;
 export declare function conf(name: string, fallback?: any, cast?: string, ctx?: any): any;
