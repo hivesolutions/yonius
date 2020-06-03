2b02063652584cbfc845bec277eccb8caad72d23
joamag <joamag@hive.pt>
2020-06-03 09:39:29 +0100

    Small fix in declaration

--- types/util/config.d.ts
+++ types/util/config.d.ts
@@ -1,2 +1,2 @@
-export declare function load(names?: Array<string>, path?: string, encoding?: string, force?: boolean, ctx?: any): Promise<any>;
+export declare function load(names?: Array<string>, path?: string, encoding?: string, force?: boolean, ctx?: any): Promise<void>;
 export declare function conf(name: string, fallback?: any, cast?: string, ctx?: any): any;
