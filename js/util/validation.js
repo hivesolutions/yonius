d8c0b3f8a7c430f579e36c20666b27d242db8613
Gabriel Candal <gcc@platforme.com>
2020-09-16 17:31:19 +0100

    Add set of validations

--- js/util/validation.js
+++ js/util/validation.js
@@ -1,5 +1,24 @@
 import { ValidationError } from "../base/error";
 
+/**
+ * The simple regex used to validate
+ * if the provided value is a "simple" one meaning
+ * that it may be used safely for URL parts
+ */
+const SIMPLE_REGEX = /^[\:\.\s\w-]+$/;
+
+/**
+ * The email regex used to validate
+ * if the provided value is in fact an email
+ */
+const EMAIL_REGEX = /^[\w\d\._%+-]+@[\w\d\.\-]+$/;
+
+/**
+ * The URL regex used to validate
+ * if the provided value is in fact an URL/URI
+ */
+const URL_REGEX = /^\w+\:\/\/([^@]+\:[^@]+@)?[^\:\/\?#]+(\:\d+)?(\/[^\?#]+)*\/?(\?[^#]*)?(#.*)?$/;
+
 export const eq = function(valueC, message = "Must be equal to %{1}") {
     const validation = (value, ctx) => {
         if (value === null) return true;
@@ -22,6 +41,55 @@ export const notEmpty = function(valueC, message = "Value is empty") {
     const validation = (value, ctx) => {
         if (value === null) return true;
         if (value.length) return true;
+        throw new ValidationError(message);
+    };
+    return validation;
+};
+
+export const isIn = function(valueC, message = "Value must be one of: %{1}") {
+    const validation = (value, ctx) => {
+        if (value === null) return true;
+        if (valueC.includes(value)) return true;
+        throw new ValidationError(message.replace("%{1}", valueC.join(", ")));
+    };
+    return validation;
+};
+
+export const isSimple = function(valueC, message = "Value contains invalid characters") {
+    const validation = (value, ctx) => {
+        if (value === null) return true;
+        if (value === "") return true;
+        if (value.match(SIMPLE_REGEX)) return true;
+        throw new ValidationError(message);
+    };
+    return validation;
+};
+
+export const isEmail = function(valueC, message = "Value is not a valid email") {
+    const validation = (value, ctx) => {
+        if (value === null) return true;
+        if (value === "") return true;
+        if (value.match(EMAIL_REGEX)) return true;
+        throw new ValidationError(message);
+    };
+    return validation;
+};
+
+export const isUrl = function(valueC, message = "Value is not a valid URL") {
+    const validation = (value, ctx) => {
+        if (value === null) return true;
+        if (value === "") return true;
+        if (value.match(URL_REGEX)) return true;
+        throw new ValidationError(message);
+    };
+    return validation;
+};
+
+export const isRegex = function(valueC, message = "Value has incorrect format") {
+    const validation = (value, ctx) => {
+        if (value === null) return true;
+        if (value === "") return true;
+        if (value.match(new RegExp(valueC))) return true;
         throw new ValidationError(message);
     };
     return validation;
