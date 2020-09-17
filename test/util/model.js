5e553ed14b21c28848049cc531a854c7db54476e
Gabriel Candal <gcc@platforme.com>
2020-09-17 10:39:15 +0100

    Proper Model#get() and raiseE

--- test/util/model.js
+++ test/util/model.js
@@ -2,13 +2,40 @@ const assert = require("assert");
 const yonius = require("../..");
 
 class MockCollection {
+    findOne(params, fields, options) {
+        this.lastCall = {
+            params: params,
+            fields: fields,
+            options: options
+        };
+        const result =
+            MockCollection.result !== undefined
+                ? MockCollection.result
+                : { id: "mock", name: "mock" };
+        MockCollection.resetResult();
+        return result;
+    }
+
     find(params, fields, options) {
         this.lastCall = {
             params: params,
             fields: fields,
             options: options
         };
-        return [];
+        const result =
+            MockCollection.result !== undefined
+                ? MockCollection.result
+                : [{ id: "mock", name: "mock" }];
+        MockCollection.resetResult();
+        return result;
+    }
+
+    static setResult(result) {
+        MockCollection.result = result;
+    }
+
+    static resetResult() {
+        MockCollection.result = undefined;
     }
 }
 
@@ -40,36 +67,76 @@ class MockModel extends yonius.ModelStore {
     }
 }
 
+describe("#get()", function() {
+    it("should allow filter", async () => {
+        await MockModel.get({
+            brand: "swear"
+        });
+        assert.deepStrictEqual(await MockModel.lastCall.params, {
+            brand: "swear"
+        });
+    });
+
+    it("should raise exception", async () => {
+        MockCollection.setResult(null);
+
+        await assert.rejects(
+            MockModel.get({
+                brand: "swear"
+            }),
+            err => {
+                assert.strictEqual(err instanceof yonius.NotFoundError, true);
+                return true;
+            }
+        );
+        assert.deepStrictEqual(await MockModel.lastCall.params, {
+            brand: "swear"
+        });
+    });
+
+    it("should ignore exception", async () => {
+        MockCollection.setResult(null);
+
+        await MockModel.get({
+            brand: "swear",
+            raiseE: false
+        });
+        assert.deepStrictEqual(await MockModel.lastCall.params, {
+            brand: "swear"
+        });
+    });
+});
+
 describe("#find()", function() {
-    it("should allow filter", () => {
-        MockModel.find({
+    it("should allow filter", async () => {
+        await MockModel.find({
             find_d: "brand:eq:swear",
             filter_operator: "$or"
         });
-        assert.deepStrictEqual(MockModel.lastCall.params, {
+        assert.deepStrictEqual(await MockModel.lastCall.params, {
             brand: "swear",
             filter_operator: "$or"
         });
     });
 
-    it("should allow filters", () => {
-        MockModel.find({
+    it("should allow filters", async () => {
+        await MockModel.find({
             find_d: ["nr1:gt:5", "nr2:lt:10"],
             filter_operator: "$and"
         });
-        assert.deepStrictEqual(MockModel.lastCall.params, {
+        assert.deepStrictEqual(await MockModel.lastCall.params, {
             nr1: { $gt: "5" },
             nr2: { $lt: "10" },
             filter_operator: "$and"
         });
     });
 
-    it("should allow likei", () => {
-        MockModel.find({
+    it("should allow likei", async () => {
+        await MockModel.find({
             find_d: "brand:likei:swear",
             filter_operator: "$or"
         });
-        assert.deepStrictEqual(MockModel.lastCall.params, {
+        assert.deepStrictEqual(await MockModel.lastCall.params, {
             brand: {
                 $options: "-i",
                 $regex: "^.*swear.*$"
@@ -78,11 +145,11 @@ describe("#find()", function() {
         });
     });
 
-    it("should allow find_s", () => {
-        MockModel.find({
+    it("should allow find_s", async () => {
+        await MockModel.find({
             find_s: "swear"
         });
-        assert.deepStrictEqual(MockModel.lastCall.params, {
+        assert.deepStrictEqual(await MockModel.lastCall.params, {
             name: {
                 $options: "",
                 $regex: "swear"
@@ -90,12 +157,12 @@ describe("#find()", function() {
         });
     });
 
-    it("should allow case-insentitive find_s", () => {
-        MockModel.find({
+    it("should allow case-insentitive find_s", async () => {
+        await MockModel.find({
             find_s: "swear",
             find_i: 1
         });
-        assert.deepStrictEqual(MockModel.lastCall.params, {
+        assert.deepStrictEqual(await MockModel.lastCall.params, {
             name: {
                 $options: "-i",
                 $regex: "swear"
@@ -103,25 +170,58 @@ describe("#find()", function() {
         });
     });
 
-    it("should allow pages and sorting", () => {
-        MockModel.find({
+    it("should allow pages and sorting", async () => {
+        await MockModel.find({
             find_d: "brand:likei:swear",
             filter_operator: "$or",
             skip: 5,
             limit: 10,
             sort: [["name", 1]]
         });
-        assert.deepStrictEqual(MockModel.lastCall.params, {
+        assert.deepStrictEqual(await MockModel.lastCall.params, {
             brand: {
                 $options: "-i",
                 $regex: "^.*swear.*$"
             },
             filter_operator: "$or"
         });
-        assert.deepStrictEqual(MockModel.lastCall.options, {
+        assert.deepStrictEqual(await MockModel.lastCall.options, {
             limit: 10,
             skip: 5,
             sort: { name: 1 }
+        });
+    });
+
+    it("should raise exception", async () => {
+        MockCollection.setResult([]);
+
+        await assert.rejects(
+            MockModel.find({
+                find_d: "brand:eq:swear",
+                filter_operator: "$or"
+            }),
+            err => {
+                assert.strictEqual(err instanceof yonius.NotFoundError, true);
+                return true;
+            }
+        );
+        assert.deepStrictEqual(await MockModel.lastCall.params, {
+            brand: "swear",
+            filter_operator: "$or"
+        });
+    });
+
+    it("should ignore exception", async () => {
+        MockCollection.setResult([]);
+
+        await MockModel.find({
+            find_d: "brand:eq:swear",
+            filter_operator: "$or",
+            raiseE: false
+        });
+        assert.deepStrictEqual(await MockModel.lastCall.params, {
+            brand: "swear",
+            filter_operator: "$or"
         });
     });
 });
