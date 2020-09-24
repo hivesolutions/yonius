8b20e7ed83f58279d48bf66cde86b8fbdcddbfce
Margarida Silva <t-ms@platforme.com>
2020-09-24 11:22:24 +0100

    Check if header key exists

--- js/api/api.js
+++ js/api/api.js
@@ -143,7 +143,7 @@ export class API extends Observable {
 
     async _handleResponse(response, errorMessage = "Problem in request") {
         let result = null;
-        if (response.headers.get("content-type").toLowerCase().startsWith("application/json")) {
+        if (response.headers.get("content-type") && response.headers.get("content-type").toLowerCase().startsWith("application/json")) {
             result = await response.json();
         } else {
             result = await response.blob();
