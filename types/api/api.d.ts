86f9d4af10d4edbc5509321d5301dee64f7b9f09
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-01-08 10:40:22 +0000

    Expose the HTTP basic methods from yonius to typescript

---
+++ types/api/api.d.ts
@@ -0,0 +1,9 @@
+export declare class API {
+    constructor(kwargs?: object)
+    async get(url: string, options?: object): any
+    async post(url: string, options?: object): any
+    async put(url: string, options?: object): any
+    async delete(url: string, options?: object): any
+    async patch(url: string, options?: object): any
+    async options(url: string, options?: object): any
+}
