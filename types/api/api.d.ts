12af79de918cfe7f48896cb0d08ee51244478d7d
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-01-08 10:49:23 +0000

    Stronger type definition

--- types/api/api.d.ts
+++ types/api/api.d.ts
@@ -1,9 +1,9 @@
 export declare class API {
-    constructor(kwargs?: object)
-    async get(url: string, options?: object): any
-    async post(url: string, options?: object): any
-    async put(url: string, options?: object): any
-    async delete(url: string, options?: object): any
-    async patch(url: string, options?: object): any
-    async options(url: string, options?: object): any
+    constructor(kwargs?: Record<string, unknown>)
+    async get(url: string, options?: Record<string, unknown>): any
+    async post(url: string, options?: Record<string, unknown>): any
+    async put(url: string, options?: Record<string, unknown>): any
+    async delete(url: string, options?: Record<string, unknown>): any
+    async patch(url: string, options?: Record<string, unknown>): any
+    async options(url: string, options?: Record<string, unknown>): any
 }
