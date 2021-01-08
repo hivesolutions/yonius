aa0a78716ac0babcdf5fe1a49d98403aec2034cb
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-01-08 15:08:44 +0000

    APIOptions

--- types/api/api.d.ts
+++ types/api/api.d.ts
@@ -1,9 +1,19 @@
 export declare class API {
     constructor(kwargs?: Record<string, unknown>)
-    get(url: string, options?: Record<string, unknown>): unknown
-    post(url: string, options?: Record<string, unknown>): unknown
-    put(url: string, options?: Record<string, unknown>): unknown
-    delete(url: string, options?: Record<string, unknown>): unknown
-    patch(url: string, options?: Record<string, unknown>): unknown
-    options(url: string, options?: Record<string, unknown>): unknown
+    get(url: string, options?: APIOptions): unknown
+    post(url: string, options?: APIOptions): unknown
+    put(url: string, options?: APIOptions): unknown
+    delete(url: string, options?: APIOptions): unknown
+    patch(url: string, options?: APIOptions): unknown
+    options(url: string, options?: APIOptions): unknown
+}
+
+type APIOptions = {
+    params?: Record<string, unknown>,
+    headers?: Record<string, unknown>,
+    kwargs?: Record<string, unknown>,
+    handle?: boolean,
+    data?: unknown,
+    dataJ?: JSON,
+    dataM?: unknown,
 }
