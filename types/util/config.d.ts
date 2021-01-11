61c1687906b355f703fc68bffcc267198d4bbe16
Gabriel Candal <gcc@platforme.com>
2021-01-11 09:12:01 +0000

    feat: use type unknown instead of any

--- types/util/config.d.ts
+++ types/util/config.d.ts
@@ -1,2 +1,2 @@
-export declare function load(names?: Array<string>, path?: string, encoding?: string, force?: boolean, ctx?: any): Promise<void>;
-export declare function conf(name: string, fallback?: any, cast?: string, ctx?: any): any;
+export declare function load(names?: Array<string>, path?: string, encoding?: string, force?: boolean, ctx?: unknown): Promise<void>;
+export declare function conf(name: string, fallback?: unknown, cast?: string, ctx?: unknown): unknown;
