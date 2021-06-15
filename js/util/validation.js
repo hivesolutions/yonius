61fab57c35fa50894210aa710aecde3b5ffb81fc
Nuno Silva <nuno_nfss96@hotmail.com>
2021-06-15 10:21:18 +0100

    fix: allow not iterable sequence

--- js/util/validation.js
+++ js/util/validation.js
@@ -153,6 +153,8 @@ export const stringEq = function(valueC, message = "Must be exactly %{1} charact
 
 export const all = function(validation) {
     const _validation = (sequence, ctx) => {
+        if (!Array.isArray(sequence)) return true;
+
         for (const value of sequence) {
             validation(value, ctx);
         }
