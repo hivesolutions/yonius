8e43ddd9a8c906487682d04d6a1567a4f682784c
Nuno Silva <nuno_nfss96@hotmail.com>
2021-06-15 10:22:16 +0100

    style: remove new line

--- js/util/validation.js
+++ js/util/validation.js
@@ -154,7 +154,6 @@ export const stringEq = function(valueC, message = "Must be exactly %{1} charact
 export const all = function(validation) {
     const _validation = (sequence, ctx) => {
         if (!Array.isArray(sequence)) return true;
-
         for (const value of sequence) {
             validation(value, ctx);
         }
