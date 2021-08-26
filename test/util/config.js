161e31d6f2609f9cf9426acc3c6ea89b10f7798b
João Magalhães <joamag@gmail.com>
2021-08-26 13:21:31 +0100

    test: add castR unit testing

--- test/util/config.js
+++ test/util/config.js
@@ -48,3 +48,38 @@ describe("#conf()", function() {
         assert.strictEqual(yonius._isValid("$include"), false);
     });
 });
+
+describe("#_castR()", function() {
+    it("should be able to cast simple types", () => {
+        let caster;
+
+        caster = yonius._castR("int");
+        assert.strictEqual(caster("12"), 12);
+        assert.strictEqual(caster("12.2"), 12);
+        assert.strictEqual(isNaN(caster("")), true);
+
+        caster = yonius._castR("float");
+        assert.strictEqual(caster("12.2"), 12.2);
+        assert.strictEqual(caster("12.22"), 12.22);
+        assert.strictEqual(isNaN(caster("")), true);
+
+        caster = yonius._castR("bool");
+        assert.strictEqual(caster("1"), true);
+        assert.strictEqual(caster("0"), false);
+        assert.strictEqual(caster("true"), true);
+        assert.strictEqual(caster("false"), false);
+        assert.strictEqual(caster("True"), true);
+        assert.strictEqual(caster("False"), false);
+        assert.strictEqual(caster(""), false);
+
+        caster = yonius._castR("list");
+        assert.deepStrictEqual(caster("1;2;3"), ["1", "2", "3"]);
+        assert.deepStrictEqual(caster("abc;def;ghi"), ["abc", "def", "ghi"]);
+        assert.deepStrictEqual(caster(""), [""]);
+
+        caster = yonius._castR("tuple");
+        assert.deepStrictEqual(caster("1;2;3"), ["1", "2", "3"]);
+        assert.deepStrictEqual(caster("abc;def;ghi"), ["abc", "def", "ghi"]);
+        assert.deepStrictEqual(caster(""), [""]);
+    });
+});
