75327b16041aa692fa69859bb6ce3fadef672408
NFSS10 <nuno_nfss96@hotmail.com>
2022-05-13 13:08:34 +0100

    feat: add `fromArrayBuffer` and `fromBlob` to FileTuple

--- types/base/struct.d.ts
+++ types/base/struct.d.ts
@@ -1,6 +1,8 @@
 export declare class FileTuple extends Array {
     static fromData(data: Uint8Array, name?: string, mime?: string): FileTuple;
     static fromString(dataString: StringConstructor, name?: string, mime?: string): FileTuple;
+    static fromArrayBuffer(arrayBuffer: ArrayBuffer, name?: string, mime?: string): FileTuple;
+    static async fromBlob(blob: Blob, name?: string, mime?: string): FileTuple;
     get name(): string;
     get mime(): string;
     get data(): Uint8Array;
