7b3a5ebae0afdd6cf74127542f4fd86787d98e90
NFSS10 <nuno_nfss96@hotmail.com>
2022-05-13 15:35:53 +0100

    feat: add `#fromString()`, `#fromArrayBuffer()` and `#fromBlob()` tests

--- test/base/struct.js
+++ test/base/struct.js
@@ -1,11 +1,63 @@
 const assert = require("assert");
 const yonius = require("../../");
 
+const { Blob } = require("buffer");
+
 describe("FileTuple", function() {
     describe("#fromData()", async function() {
         it("should be able to create a simple file tuple objects", () => {
             const fileTuple = yonius.FileTuple.fromData(
                 new Uint8Array(),
+                "hello.txt",
+                "text/plain"
+            );
+            assert.notStrictEqual(fileTuple, null);
+            assert.strictEqual(fileTuple.constructor, yonius.FileTuple);
+            assert.strictEqual(fileTuple.name, "hello.txt");
+            assert.strictEqual(fileTuple.mime, "text/plain");
+            assert.strictEqual(fileTuple.data.byteLength, 0);
+            assert.strictEqual(fileTuple instanceof yonius.FileTuple, true);
+            assert.strictEqual(fileTuple instanceof Array, true);
+            assert.strictEqual(Array.isArray(fileTuple), true);
+        });
+    });
+
+    describe("#fromString()", async function() {
+        it("should be able to create a simple file tuple objects", () => {
+            const fileTuple = yonius.FileTuple.fromString("hello", "hello.txt", "text/plain");
+            assert.notStrictEqual(fileTuple, null);
+            assert.strictEqual(fileTuple.constructor, yonius.FileTuple);
+            assert.strictEqual(fileTuple.name, "hello.txt");
+            assert.strictEqual(fileTuple.mime, "text/plain");
+            assert.strictEqual(fileTuple.data.byteLength, 5);
+            assert.strictEqual(fileTuple instanceof yonius.FileTuple, true);
+            assert.strictEqual(fileTuple instanceof Array, true);
+            assert.strictEqual(Array.isArray(fileTuple), true);
+        });
+    });
+
+    describe("#fromArrayBuffer()", async function() {
+        it("should be able to create a simple file tuple objects", () => {
+            const fileTuple = yonius.FileTuple.fromArrayBuffer(
+                new ArrayBuffer(),
+                "hello.txt",
+                "text/plain"
+            );
+            assert.notStrictEqual(fileTuple, null);
+            assert.strictEqual(fileTuple.constructor, yonius.FileTuple);
+            assert.strictEqual(fileTuple.name, "hello.txt");
+            assert.strictEqual(fileTuple.mime, "text/plain");
+            assert.strictEqual(fileTuple.data.byteLength, 0);
+            assert.strictEqual(fileTuple instanceof yonius.FileTuple, true);
+            assert.strictEqual(fileTuple instanceof Array, true);
+            assert.strictEqual(Array.isArray(fileTuple), true);
+        });
+    });
+
+    describe("#fromBlob()", async function() {
+        it("should be able to create a simple file tuple objects", async () => {
+            const fileTuple = await yonius.FileTuple.fromBlob(
+                new Blob(),
                 "hello.txt",
                 "text/plain"
             );
