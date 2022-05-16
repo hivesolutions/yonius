a23a8cb4bf76bbc87eec1dbe0f6d7c77f8b123ac
João Magalhães <joamag@gmail.com>
2022-05-16 09:35:22 +0100

    fix: typescript declaration

--- types/base/struct.d.ts
+++ types/base/struct.d.ts
@@ -2,7 +2,7 @@ export declare class FileTuple extends Array {
     static fromData(data: Uint8Array, name?: string, mime?: string): FileTuple;
     static fromString(dataString: StringConstructor, name?: string, mime?: string): FileTuple;
     static fromArrayBuffer(arrayBuffer: ArrayBuffer, name?: string, mime?: string): FileTuple;
-    static async fromBlob(blob: Blob, name?: string, mime?: string): FileTuple;
+    static fromBlob(blob: Blob, name?: string, mime?: string): Promise<FileTuple>;
     get name(): string;
     get mime(): string;
     get data(): Uint8Array;
